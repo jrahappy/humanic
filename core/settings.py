8df1ebee46d44977c7c8a7c311f0aea82cc06eb9
jrahappy <william.bh.lee@gmail.com>
2024-09-09 09:54:07 -0400

    static

--- core/settings.py
+++ core/settings.py
@@ -168,17 +168,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_ROOT = BASE_DIR / "media"
 
-STATIC_URL = "staticfiles/"
+STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static/"]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def index(request):
-    return render(request, "web/index.html")
+    return redirect("account_login")
+    # return render(request, "web/index.html")
