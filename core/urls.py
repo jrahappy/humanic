9803be4890eed7f6b9c477ea7e7dbf64f8257e3c
jrahappy <william.bh.lee@gmail.com>
2024-09-09 09:59:59 -0400

    add urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

--- core/urls.py
+++ core/urls.py
@@ -33,4 +33,7 @@ urlpatterns = [
     path("product/", include("product.urls")),
     path("importdata/", include("importdata.urls")),
     path("dogfoot/", include("dogfoot.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
