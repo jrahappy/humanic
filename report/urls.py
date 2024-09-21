724d7b3dc4db09790fb05ca9494a1777c1a04ce4
jrahappy <william.bh.lee@gmail.com>
2024-09-21 02:29:18 -0400

    Basic report start (draft)

--- report/urls.py
+++ report/urls.py
@@ -4,4 +4,15 @@ from . import views
 app_name = "report"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("report_period/", views.report_period, name="report_period"),
+    path(
+        "report_period_month/<int:ayear>/<int:amonth>/",
+        views.report_period_month,
+        name="report_period_month",
+    ),
+    path(
+        "report_period_month_radiologist/<int:ayear>/<int:amonth>/<str:radio>/",
+        views.report_period_month_radiologist,
+        name="report_period_month_radiologist",
+    ),
 ]
--- report/views.py
+++ report/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from minibooks.models import UploadHistory, ReportMaster
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .filters import ReportFilter
+from django.db.models import Count, Sum
 
 
 def index(request):
@@ -21,3 +22,59 @@ def index(request):
     context = {"rmaster": rmaster, "filter": report_filter}
 
     return render(request, "report/index.html", context)
+
+
+def report_period(request):
+
+    buttons_year_month = (
+        UploadHistory.objects.filter(is_deleted=False)
+        .values("ayear", "amonth")
+        .distinct()
+        .order_by("ayear", "amonth")
+    )
+
+    context = {"buttons_year_month": buttons_year_month}
+
+    return render(request, "report/report_period.html", context)
+
+
+def report_period_month(request, ayear, amonth):
+    rpms = (
+        ReportMaster.objects.filter(ayear=ayear, amonth=amonth)
+        .values("radiologist")  # Use the related field's real_name
+        .annotate(
+            total_price=Sum("readprice"),
+            total_provider=Sum("pay_to_provider"),
+            total_human=Sum("pay_to_human"),
+            total_cases=Count("case_id"),
+        )
+    )
+    count_rpms = rpms.count()
+
+    context = {"rpms": rpms, "count_rpms": count_rpms, "ayear": ayear, "amonth": amonth}
+
+    return render(request, "report/report_period_month.html", context)
+
+
+def report_period_month_radiologist(request, ayear, amonth, radio):
+    rpms = (
+        ReportMaster.objects.filter(ayear=ayear, amonth=amonth, radiologist=radio)
+        .values("apptitle")
+        .annotate(
+            total_price=Sum("readprice"),
+            total_provider=Sum("pay_to_provider"),
+            total_human=Sum("pay_to_human"),
+            total_cases=Count("case_id"),
+        )
+    )
+    count_rpms = rpms.count()
+
+    context = {
+        "rpms": rpms,
+        "count_rpms": count_rpms,
+        "ayear": ayear,
+        "amonth": amonth,
+        "radio": radio,
+    }
+
+    return render(request, "report/report_period_month_radiologist.html", context)
