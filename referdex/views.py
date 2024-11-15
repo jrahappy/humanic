ff2ce5f1a9257a0238aae5365c02c8a801b65472
jrahappy <william.bh.lee@gmail.com>
2024-11-15 00:17:41 -0500

    add stat on referdex

--- referdex/views.py
+++ referdex/views.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from minibooks.models import MagamMaster, ReportMasterStat, ReportMaster
 from accounts.models import CustomUser, ProductionTarget, Profile, WorkHours, Holidays
 from datetime import date, datetime
+from utils.base_func import get_specialty_choices
 
 
 # Create your views here.
@@ -68,6 +69,88 @@ def index(request):
         )
     )
 
+    stat_values = defaultdict(dict)
+    specialties = get_specialty_choices()
+
+    total_provider_a = CustomUser.objects.filter(
+        is_doctor=True, is_active=True, profile__contract_status="A"
+    ).count()
+    total_provider_p = CustomUser.objects.filter(
+        is_doctor=True, is_active=True, profile__contract_status="P"
+    ).count()
+
+    for spct in specialties:
+        specialty_name = spct[0]
+        # print("Specialty Name: ", specialty_name)
+        # Active providers by specialty
+        count_provider_a = CustomUser.objects.filter(
+            profile__specialty2=specialty_name,
+            is_doctor=True,
+            is_active=True,
+            profile__contract_status="A",
+        ).count()
+        # PartTime providers by specialty
+        count_provider_p = CustomUser.objects.filter(
+            profile__specialty2=specialty_name,
+            is_doctor=True,
+            is_active=True,
+            profile__contract_status="P",
+        ).count()
+
+        # Total CT 희망 갯수 합계
+        sum_ct_target = (
+            ProductionTarget.objects.filter(
+                work_weekday=selected_weekday,
+                modality="ct",
+                user__profile__specialty2=specialty_name,
+                user__is_doctor=True,
+                user__is_active=True,
+            ).aggregate(Sum("target_value"))["target_value__sum"]
+            or 0
+        )
+
+        sum_mr_target = (
+            ProductionTarget.objects.filter(
+                work_weekday=selected_weekday,
+                modality="mr",
+                user__profile__specialty2=specialty_name,
+                user__is_doctor=True,
+                user__is_active=True,
+            ).aggregate(Sum("target_value"))["target_value__sum"]
+            or 0
+        )
+
+        sum_mg_target = (
+            ProductionTarget.objects.filter(
+                work_weekday=selected_weekday,
+                modality="mg",
+                user__profile__specialty2=specialty_name,
+                user__is_doctor=True,
+                user__is_active=True,
+            ).aggregate(Sum("target_value"))["target_value__sum"]
+            or 0
+        )
+
+        sum_cr_target = (
+            ProductionTarget.objects.filter(
+                work_weekday=selected_weekday,
+                modality="cr",
+                user__profile__specialty2=specialty_name,
+                user__is_doctor=True,
+                user__is_active=True,
+            ).aggregate(Sum("target_value"))["target_value__sum"]
+            or 0
+        )
+
+        stat_values[specialty_name] = {
+            "count_provider_a": count_provider_a,
+            "count_provider_p": count_provider_p,
+            "sum_ct_target": sum_ct_target,
+            "sum_mr_target": sum_mr_target,
+            "sum_mg_target": sum_mg_target,
+            "sum_cr_target": sum_cr_target,
+        }
+
     # Group providers by specialty with additional data
     grouped_by_specialty = defaultdict(list)
     for provider in selected_providers:
@@ -75,14 +158,20 @@ def index(request):
         grouped_by_specialty[specialty_name].append(
             {
                 "provider": provider,
+                "iid": provider.id,
                 "real_name": provider.profile.real_name,
+                "contract_status": provider.profile.contract_status,
                 "production_targets": provider.filtered_production_targets,
                 "workhours": provider.filtered_workhours,
             }
         )
 
     context = {
+        "today": date.today(),
+        "total_provider_a": total_provider_a,
+        "total_provider_p": total_provider_p,
         "selected_date": selected_date,
         "grouped_by_specialty": dict(grouped_by_specialty),
+        "stat_values": dict(stat_values),
     }
     return render(request, "referdex/index.html", context)
