19f08c7bbc8016baa997dc98e5dfcb619137c80c
jrahappy <william.bh.lee@gmail.com>
2025-06-16 10:40:23 -0400

    debug logging at setting

--- core/settings.py
+++ core/settings.py
@@ -277,67 +277,22 @@ CELERY_TIMEZONE = "Asia/Seoul"
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "filters": {
-        "require_debug_false": {
-            "()": "django.utils.log.RequireDebugFalse",
-        },
-        "require_debug_true": {
-            "()": "django.utils.log.RequireDebugTrue",
-        },
-    },
-    "formatters": {
-        "django.server": {
-            "()": "django.utils.log.ServerFormatter",
-            "format": "[{server_time}] {message}",
-            "style": "{",
-        },
-        "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
-    },
     "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "/path/to/logs/humanrad.log",
+        },
         "console": {
             "level": "INFO",
-            "filters": ["require_debug_true"],
             "class": "logging.StreamHandler",
-        },
-        "django.server": {
-            "level": "INFO",
-            "class": "logging.StreamHandler",
-            "formatter": "django.server",
-        },
-        "mail_admins": {
-            "level": "ERROR",
-            "filters": ["require_debug_false"],
-            "class": "django.utils.log.AdminEmailHandler",
-        },
-        "file": {
-            "level": "INFO",
-            "filters": ["require_debug_false"],
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": BASE_DIR / "logs/mysite.log",
-            "maxBytes": 1024 * 1024 * 5,  # 5 MB
-            "backupCount": 5,
-            "formatter": "standard",
         },
     },
     "loggers": {
         "django": {
-            "handlers": ["console", "mail_admins", "file"],
-            "level": "INFO",
+            "handlers": ["file", "console"],
+            "level": "DEBUG",
+            "propagate": True,
         },
-        "django.server": {
-            "handlers": ["django.server"],
-            "level": "INFO",
-            "propagate": False,
-        },
-        # "celery": {
-        #     "handlers": ["file_celery", "console"],
-        #     "level": "INFO",
-        #     "propagate": False,
-        # },
-        # "core": {
-        #     "handlers": ["file_celery", "console"],
-        #     "level": "INFO",
-        #     "propagate": False,
-        # },
     },
 }
